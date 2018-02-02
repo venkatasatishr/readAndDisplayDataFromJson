5740bb84bf7cbc6624992e62be2d1b8afdb800b1
venkatasatishr <satish.rakonda@india.semanticbits.com>
2018-02-02 09:26:26 +0530

    work done at home

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+var userData = [],i;
+var fn,ln,dob,email,mobile;
 $.getJSON( "Users.json", function(json){
     //generate a dynamic table using json
     //build userThreeDisplay() method with required args.. userThreeDisplay(json[0].firstname, lastname, dob, )
+   // var userData = JSON.parse(json);
+    for(var i = 0;i<json.length;i++){
+        userData.push(json[i]);
+    }
+    console.log(json);
 
-    var userData = json;
-    console.log(userData);
     var col = [];
     for (var i = 0; i < json.length; i++) {
         for (var key in json[i]) {
@@ -13,13 +18,8 @@ $.getJSON( "Users.json", function(json){
                 break;
             }
         }
-
     }
-    //col.push("View");
-    /*console.log("----------------------------");
-    for(var i = 0; i<col.length;i++ )
-        console.log(col[i]);*/
-    //creating a dynamic table
+     //creating a dynamic table
     var table = document.createElement("table");
     table.setAttribute("class", "table table-hover");
     var tr = table.insertRow(-1);
@@ -32,25 +32,26 @@ $.getJSON( "Users.json", function(json){
     }
 
     var buttonArr = [];
-    for(var i = 0;i<json.length;i++){
+    var target = "modal",btnId= "btn";
+
+    for(i = 0;i<json.length;i++){
+        target = "#modal";
+        btnId = "btn"
         buttonArr[i]=document.createElement("button");
         buttonArr[i].setAttribute("type","button");
+        btnId = btnId+i;
+        buttonArr[i].setAttribute("id",btnId);
         buttonArr[i].setAttribute("class","btn btn-primary");
         buttonArr[i].setAttribute("data-toggle","modal");
-        buttonArr[i].setAttribute("data-target","#modalOne");
-        buttonArr[i].setAttribute("onclick","userOneDisplay(userData[i].FirstName,userData[i].LastName,userData[i].DOB,userData[i].E-Mail,userData[i].Mobile)");
+        target = target +i;
+        //console.log(target+"-->");
+        buttonArr[i].setAttribute("data-target",target);
+        fn = userData[i].FirstName;ln = userData[i].LastName;dob = userData[i].DOB;email = userData[i].eMail; mobile = userData[i].Mobile;
+        buttonArr[i].setAttribute("onclick","userOneDisplay(fn, ln,dob,email,mobile)");
         buttonArr[i].innerHTML= "View";
-
-    //userOneDisplay(json[i].FirstName,json[i].LastName,json[i].DOB,json[i].E-Mail,json[i].Mobile)
     }
-    /*var button = document.createElement("button");
-    button.setAttribute("type","button");
-    button.setAttribute("class","btn btn-primary");
-    button.setAttribute("data-toggle","modal");
-    button.setAttribute("data-target","");
-    button.setAttribute("onclick","userOneDisplay()");
-    button.innerHTML= "View";*/
-    var id = "sub",ele,bId=[];
+    //adding table data <td>
+    var id = "sub",ele,bId=[];//ele for getting button object
     for (var i = 0; i < json.length; i++) {
         id = "sub";
         tr = table.insertRow(-1);
@@ -59,25 +60,19 @@ $.getJSON( "Users.json", function(json){
             var tabCell = tr.insertCell(-1);
             tabCell.innerHTML = json[i][col[j]];
             console.log(tabCell.innerHTML+"------------");
-            if(tabCell.innerHTML == "") {
+            if(tabCell.innerHTML == "undefined") {
                 id = id+i;
                 bId.push(id);
                 tabCell.setAttribute("id",id);
-                //ele = document.getElementById(id);
-                //ele.appendChild(button);
-
-                //tabCell.append("button", "View");
             }
-
         }
     }
-
-
 
     // adding dynamic table to html
     var divContainer = document.getElementById("displayTable");
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
+
     //adding buttons into dynamic table
     for(var i = 0;i<bId.length;i++){
         ele= document.getElementById(bId[i]);
@@ -87,11 +82,12 @@ $.getJSON( "Users.json", function(json){
 });
 
 function userOneDisplay(arg1, arg2, arg3, arg4, arg5){
-    console.log(document.getElementById("fn1"));
-    document.getElementById("userOne").innerText = arg1;
-    document.getElementById("m1fn").innerText = arg1;
-    document.getElementById("m1ln").innerText = arg2;
-    document.getElementById("m1dob").innerText = arg3;
-    document.getElementById("m1email").innerText = arg4;
-    document.getElementById("m1mobile").innerText = arg5;
+    //window.location.href = "displayUserDetails.html";
+    //console.log(document.getElementById("fn1"));
+    //document.getElementById("userOne").innerText = arg1;
+    document.getElementById("m1fn").innerHTML= arg1;
+    document.getElementById("m1ln").innerHTML = arg2;
+    document.getElementById("m1dob").innerHTML = arg3;
+    document.getElementById("m1email").innerHTML = arg4;
+    document.getElementById("m1mobile").innerHTML = arg5;
 }
